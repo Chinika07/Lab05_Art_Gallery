10e0199b8622092cf4ae7151a66d05f05fbc33d3
Chinika charles <chinikacharles@chinikas-mbp.home>
2023-01-25 15:17:56 +0000

    first commit

---
+++ src/main/java/Gallery.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Gallery {
+
+    private String name;
+    private int till;
+    private ArrayList<Artwork> stock;
+
+    //constructor
+
+    public Gallery(String name){
+        this.name = name;
+        this.till = 0;
+        this.stock = new ArrayList<>();
+
+    }
+
+    //getters and setters
+    public String getName(){
+
+    }
+
+
+
+
+}
